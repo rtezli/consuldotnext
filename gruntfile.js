c1660b46d73bd530f0c4d15840c7fd1c51072220
Robert Tezli <robert@pixills.com>
2016-01-24 13:50:12 +0100

    updated deps

--- gruntfile.js
+++ gruntfile.js
@@ -27,7 +27,7 @@ module.exports = function(grunt) {
         command: 'export PIXILLS_CONSUL_CLIENT_BUILD_CONFIG=Release'
       },
       restore: {
-        command: 'dnu restore'
+        command: 'dnu restore --quiet -f https://www.nuget.org/api/v2'
       },
       build_app: {
         options: {
