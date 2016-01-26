1ae94f378e33dd57c9e078a0d69da49cce07e871
Robert Tezli <robert@pixills.com>
2016-01-26 22:05:30 +0100

    made everything vs compatible

--- gruntfile.js
+++ gruntfile.js
@@ -1,96 +1,96 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  require('load-grunt-tasks')(grunt);
+    require('load-grunt-tasks')(grunt);
 
-  var constants = {
-    'binDir': './bin',
-    'tmpDir': './tmp',
-    'appDir': './Pixills.Consul.Client',
-    'testsDir': './Pixills.Consul.Client.Tests',
-    'release': 'Release',
-    'debug': 'Debug',
-    'framework': 'dnxcore50',
-    'config': 'Debug'
-  };
+    var constants = {
+        'binDir': './bin',
+        'tmpDir': './tmp',
+        'appDir': './Pixills.Consul.Client',
+        'testsDir': './Pixills.Consul.Client.Tests',
+        'release': 'Release',
+        'debug': 'Debug',
+        'framework': 'dnxcore50',
+        'config': 'Debug'
+    };
 
-  grunt.initConfig({
-    const: constants,
-    watch: {
-      files: ['<%= const.appDir %>/**/*.cs', '<%= const.testsDir %>/**/*.cs'],
-      tasks: ['build']
-    },
-    shell: {
-      export_debug: {
-        command: 'export PIXILLS_CONSUL_CLIENT_BUILD_CONFIG=Debug'
-      },
-      export_release: {
-        command: 'export PIXILLS_CONSUL_CLIENT_BUILD_CONFIG=Release'
-      },
-      restore: {
-        command: 'dnu restore'
-      },
-      build_app: {
-        options: {
-          execOptions: {
-            cwd: '<%= const.appDir %>'
-          }
+    grunt.initConfig({
+        const: constants,
+        watch: {
+            files: ['<%= const.appDir %>/**/*.cs', '<%= const.testsDir %>/**/*.cs'],
+            tasks: ['build']
         },
-        command: 'dnu build --configuration <%= const.config %> --quiet'
-      },
-      build_tests: {
-        options: {
-          execOptions: {
-            cwd: '<%= const.testsDir %>'
-          }
+        shell: {
+            export_debug: {
+                command: 'export PIXILLS_CONSUL_CLIENT_BUILD_CONFIG=Debug'
+            },
+            export_release: {
+                command: 'export PIXILLS_CONSUL_CLIENT_BUILD_CONFIG=Release'
+            },
+            restore: {
+                command: 'dnu restore'
+            },
+            build_app: {
+                options: {
+                    execOptions: {
+                        cwd: '<%= const.appDir %>'
+                    }
+                },
+                command: 'dnu build --configuration <%= const.config %> --quiet'
+            },
+            build_tests: {
+                options: {
+                    execOptions: {
+                        cwd: '<%= const.testsDir %>'
+                    }
+                },
+                command: 'dnu build --configuration <%= const.config %> --quiet'
+            },
+            test: {
+                options: {
+                    execOptions: {
+                        cwd: '<%= const.testsDir %>'
+                    }
+                },
+                command: 'dnx test'
+            },
+            pack: {
+                options: {
+                    execOptions: {
+                        cwd: '<%= const.appDir %>'
+                    }
+                },
+                command: 'dnu pack --framework <%= const.framework %> --configuration <%= const.config %>'
+            }
         },
-        command: 'dnu build --configuration <%= const.config %> --quiet'
-      },
-      test: {
-        options: {
-          execOptions: {
-            cwd: '<%= const.testsDir %>'
-          }
+        copy: {
+            binaries: {
+                files: [{
+                    expand: true,
+                    src: ['<%= const.tmpDir %>/**/*.dll', '<%= const.tmpDir %>/**/*.pdb', '!<%= const.tmpDir %>/**/*.Tests.dll', '<%= const.tmpDir %>/**/*.nupkg', '!<%= const.tmpDir %>/**/*.symbols.nupkg'],
+                    dest: '<%= const.binDir %>',
+                    flatten: true
+                }]
+            }
         },
-        command: 'dnx test'
-      },
-      pack: {
-        options: {
-          execOptions: {
-            cwd: '<%= const.appDir %>'
-          }
-        },
-        command: 'dnu pack --framework <%= const.framework %> --configuration <%= const.config %>'
-      }
-    },
-    copy: {
-      binaries: {
-        files: [{
-          expand: true,
-          src: ['<%= const.tmpDir %>/**/*.dll', '<%= const.tmpDir %>/**/*.pdb', '!<%= const.tmpDir %>/**/*.Tests.dll', '<%= const.tmpDir %>/**/*.nupkg', '!<%= const.tmpDir %>/**/*.symbols.nupkg'],
-          dest: '<%= const.binDir %>',
-          flatten: true
-        }]
-      }
-    },
-    clean: {
-      bin: {
-        options: {
-          force: true
-        },
-        src: ['<%= const.binDir %>']
-      },
-      temp: {
-        options: {
-          force: true
-        },
-        src: ['<%= const.tmpDir %>']
-      }
-    }
-  });
+        clean: {
+            bin: {
+                options: {
+                    force: true
+                },
+                src: ['<%= const.binDir %>']
+            },
+            temp: {
+                options: {
+                    force: true
+                },
+                src: ['<%= const.tmpDir %>']
+            }
+        }
+    });
 
-  grunt.registerTask('restore', ['shell:restore']);
-  grunt.registerTask('build', ['clean:bin', 'shell:build_app', 'shell:build_tests', 'shell:pack', 'copy', 'clean:temp']);
-  grunt.registerTask('test', ['shell:test']);
-  grunt.registerTask('ct', ['build', 'test', 'watch']);
-  grunt.registerTask('default', ['build']);
+    grunt.registerTask('restore', ['shell:restore']);
+    grunt.registerTask('build', ['clean:bin', 'shell:build_app', 'shell:build_tests', 'shell:pack', 'copy', 'clean:temp']);
+    grunt.registerTask('test', ['shell:test']);
+    grunt.registerTask('ct', ['build', 'test', 'watch']);
+    grunt.registerTask('default', ['build']);
 };
