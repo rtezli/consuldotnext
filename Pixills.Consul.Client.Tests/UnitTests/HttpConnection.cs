30eb15b81b66950ce0e7fb6b3013e92e8356f86b
Robert Tezli <robert@pixills.com>
2016-01-27 18:55:25 +0100

    updated tests

--- Pixills.Consul.Client.Tests/UnitTests/HttpConnection.cs
+++ Pixills.Consul.Client.Tests/UnitTests/HttpConnection.cs
@@ -1,51 +1,99 @@
 using Xunit;
 using Pixills.Net.Http;
-using Pixills.Consul.Client;
+using System;
 
 namespace Pixills.Consul.Client.Tests
 {
     public class HttpConnection
     {
+        private IHttpClient _client;
+
+        public HttpConnection()
+        {
+            _client = new HttpClient();
+        }
+
+        [Fact]
+        public void HttpConnection_Constructor_CallWithInvalidUrl_ShouldThrowException()
+        {
+            Assert.Throws<UriFormatException>(() =>
+            {
+                new Consul.Client.HttpConnection("this is an invalid URL", _client, true, 5);
+            });
+        }
+
+        [Fact]
+        public void HttpConnection_Constructor_CallWithEmptyUrl_ShouldThrowException()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                new Consul.Client.HttpConnection("", _client, true, 5);
+            });
+        }
+
+        [Fact]
+        public void HttpConnection_Constructor_CallWithNullUrl_ShouldThrowException()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                new Consul.Client.HttpConnection(null, _client, true, 5);
+            });
+        }
+
+        [Fact]
+        public void HttpConnection_Constructor_CallWithNullClient_ShouldThrowException()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                new Consul.Client.HttpConnection("localhost:1337", null, true, 5);
+            });
+        }
+
         [Fact]
         public void HttpConnection_Constructor_CallWithValidArgs_ShouldNotThrowException()
         {
-            Assert.Equal(true, false);
+            var con = new Consul.Client.HttpConnection("localhost:1337", _client, true, 5);
         }
 
         [Fact]
         public void HttpConnection_Constructor_CallWithLeastParameters_ShouldNotThrowException()
         {
-            Assert.Equal(true, false);
+            var con = new Consul.Client.HttpConnection("localhost:1337", _client);
         }
 
         [Fact]
         public void HttpConnection_Constructor_CallWithHostWithHttpPrefix_ShouldRemoveHttpPrefix()
         {
-            Assert.Equal(true, false);
+            var con = new Consul.Client.HttpConnection("http://localhost:1337", _client);
+            Assert.Equal(new Uri("http://localhost:1337/v1"), con.BaseAddress);
         }
 
         [Fact]
         public void HttpConnection_Constructor_CallWithTlsFalse_ShouldAddHttpPrefix()
         {
-            Assert.Equal(true, false);
+            var con = new Consul.Client.HttpConnection("localhost:1337", _client);
+            Assert.Equal(new Uri("http://localhost:1337/v1"), con.BaseAddress);
         }
 
         [Fact]
         public void HttpConnection_Constructor_CallWithTlsTrue_ShouldAddHttpsPrefix()
         {
-            Assert.Equal(true, false);
+            var con = new Consul.Client.HttpConnection("localhost:1337", _client, true);
+            Assert.Equal(new Uri("https://localhost:1337/v1"), con.BaseAddress);
         }
 
         [Fact]
         public void HttpConnection_Constructor_CallWithTimeout0_ShouldNotAddTimeout()
         {
-            Assert.Equal(true, false);
+            var con = new Consul.Client.HttpConnection("localhost:1337", _client, true, 0);
+            Assert.Equal(0, (int)con.Timeout);
         }
 
         [Fact]
         public void HttpConnection_Constructor_CallWithTimeoutGreater0_ShouldAddTimeout()
         {
-            Assert.Equal(true, false);
+            var con = new Consul.Client.HttpConnection("localhost:1337", _client, true, 5);
+            Assert.Equal(5, (int)con.Timeout);
         }
     }
 }
--- Pixills.Consul.Client/HttpConnection.cs
+++ Pixills.Consul.Client/HttpConnection.cs
@@ -3,36 +3,70 @@ using System.Text.RegularExpressions;
 using Newtonsoft.Json;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Collections.Generic;
 using Pixills.Net.Http;
 
 namespace Pixills.Consul.Client
 {
     public class HttpConnection
     {
-        public IHttpClient Client { get; }
+        private readonly IHttpClient _client;
         public string Host { get; } = "localhost";
         public string ApiVersion { get; } = "v1";
 
+        public uint Timeout
+        {
+            get
+            {
+                return (uint)_client.Timeout.Seconds;
+            }
+        }
+
+        public Uri BaseAddress
+        {
+            get
+            {
+                return _client.BaseAddress;
+            }
+        }
+
         public HttpConnection(string consulHostOrIp, IHttpClient client, bool useTls = false, uint timeoutInSeconds = 0)
         {
-            Client = client;
-            var match = Regex.Match(consulHostOrIp.ToLower(),"^http(s)?://");
+            if (string.IsNullOrWhiteSpace(consulHostOrIp))
+            {
+                throw new ArgumentException("Host or IP can not be emtpy");
+            }
+
+            if (client == null)
+            {
+                throw new ArgumentException("Client can not be null");
+            }
+
+            _client = client;
+            var match = Regex.Match(consulHostOrIp.ToLower(), "^http(s)?://");
             if (match.Success)
             {
                 consulHostOrIp = consulHostOrIp.Remove(match.Index, match.Length);
             }
+
+            try
+            {
+                _client.BaseAddress = new Uri($"{(useTls ? "https" : "http")}://{consulHostOrIp}/{ApiVersion}");
+            }
+            catch (UriFormatException e)
+            {
+                throw e;
+            }
+
             if (timeoutInSeconds > 0)
             {
-                Client.Timeout = TimeSpan.FromSeconds(timeoutInSeconds);
+                _client.Timeout = TimeSpan.FromSeconds(timeoutInSeconds);
             }
-            Client.BaseAddress = new Uri($"{(useTls ? "https" : "http")}://{consulHostOrIp}/{ApiVersion}");
         }
 
         public async Task<T> Get<T>(string url)
         {
             var option = new HttpCompletionOption();
-            var response = await Client.GetAsync(url, option);
+            var response = await _client.GetAsync(url, option);
             var contentString = await response.Content.ReadAsStringAsync();
             return JsonConvert.DeserializeObject<T>(contentString);
         }
@@ -41,14 +75,14 @@ namespace Pixills.Consul.Client
         {
             return new Task(async () =>
             {
-                await Client.PutAsync(url, new StringContent(Serializer.Serialize(obj)));
+                await _client.PutAsync(url, new StringContent(Serializer.Serialize(obj)));
             });
         }
 
         public void Dispose()
         {
-            Client.CancelPendingRequests();
-            Client.Dispose();
+            _client.CancelPendingRequests();
+            _client.Dispose();
         }
     }
 }
